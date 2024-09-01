b499c8b200ac034db80f26185ab3d11dcb6174f2
Ravi Sharma <ravi701107@gmail.com>
2024-09-01 22:27:48 +0530

    fix user page

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import apiService from "../../utils/axiosInstance";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    apiService.post("/auth/login", { email, password }).then((res) => {
+      if (res) {
+        localStorage.setItem(`token`, res.data?.data?.accessToken);
+        navigate("/dashboard");
+      }
+    });
+  };
 
   return (
     <div className="min-h-screen bg-[#085946] flex flex-col gap-8 items-center justify-center">
       <h2 className="text-white text-2xl font-bold">Wholesome Admin</h2>
-      <form className="bg-[#fff] w-[25rem] p-4 rounded-lg flex flex-col gap-4">
+      <form
+        onSubmit={(e) => onSubmit(e)}
+        className="bg-[#fff] w-[25rem] p-4 rounded-lg flex flex-col gap-4"
+      >
         <div className="flex flex-col items-start">
           <label htmlFor="">Enter email</label>
           <input
             className="w-[100%] outline-none border-2 p-2 rounded-md"
-            type="text"
+            type="email"
             name=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id=""
             placeholder=""
           />
@@ -24,13 +42,14 @@ const Login = () => {
             className="w-[100%] outline-none border-2 p-2 rounded-md"
             type="password"
             name=""
+            onChange={(e) => setPassword(e.target.value)}
             id=""
             placeholder=""
           />
         </div>
         <div className="">
           <button
-            onClick={() => navigate("/dashboard")}
+            type="submit"
             className="bg-[#085946] text-white w-[100%] rounded-lg p-2"
           >
             Login
